be55998814672e99ed9359f807958644d260f9ff
Jeremy Roßbach <jeremy1899@web.de>
2024-03-15 15:09:02 +0100

    progress

--- script.js
+++ script.js
@@ -1,51 +1,51 @@
-let currentPokemon;
-
-
 async function loadPokemon() {
-    let url = 'https://pokeapi.co/api/v2/pokemon/bulbasaur';
-    let response = await fetch(url);
-    currentPokemon = await response.json();
+    for (let i = 1; i < 25; i++) {
+        let url = 'https://pokeapi.co/api/v2/pokemon/' + `${i}`;
+        let response = await fetch(url);
+        currentPokemon = await response.json();
 
-    console.log('loaded Pokemon', currentPokemon);
-
-    renderPokemonInfo();
+        console.log(currentPokemon);
+    }
+    renderPokemon();
 }
 
 
-function renderPokemonInfo() {
-    let pokemonCard = document.getElementById('pokemon-card');
-    let PokemonType1 = currentPokemon['types'][0]['type']['name'];
-    let PokemonType2 = currentPokemon['types'][1]['type']['name'];
+function renderPokemon() {
+    let pokemonCard = document.getElementById('pokemon-cards');
 
-    pokemonCard.innerHTML += `
-        <div id="cardContent">
+    for (let i = 0; i < 24; i++) {
+        
+        pokemonCard.innerHTML += `
+        <div id="cardContent${i}" class="cardContent" onclick="showPokemonInfo()">
             <div id="header">
                 <h2>${currentPokemon['name']}</h2> 
                 <b>#${currentPokemon['order']}</b>
             </div>
-
+        
             <div id="imagePosition">
                 <div id="types">
-                    <div id="type">${PokemonType1}</div>
-                    <div id="type">${PokemonType2}</div>
+                    <div id="type">${currentPokemon['types'][0]['type']['name']}</div>
                 </div>
-                <img id="image" src="./img/bulbasaur.png">
+                <img src="${currentPokemon['sprites']['front_default']}">
             </div>
         </div>
     `;
+    }
 }
 
 
+
+
+
+
 function showPokemonInfo() {
-    let PokemonType1 = currentPokemon['types'][0]['type']['name'];
-    let PokemonType2 = currentPokemon['types'][1]['type']['name'];
-    document.getElementById('pokemon-card').style.display = "none";
+    document.getElementById('pokemon-cards').style.display = "none";
 
     let pokemonInfo = document.getElementById('pokemon-info');
 
     pokemonInfo.innerHTML = `
         <div id="infoContent">
-            <button onclick="goBack()">X</button>
+            <button id=xButton onclick="goBack()">x</button>
 
             <div id="infoHeader">
                 <h1>${currentPokemon['name']}</h1>
@@ -53,15 +53,84 @@ function showPokemonInfo() {
             </div>
 
             <div id="infoTypes">
-                <div id="type">${PokemonType1}</div>
-                <div id="type">${PokemonType2}</div>
+                <div id="type">${currentPokemon['types'][0]['type']['name']}</div>
             </div>
 
             <div class="center">
-                <img id="infoImage" src="./img/bulbasaur.png">
+                <img src="${currentPokemon['sprites']['front_default']}">
             </div>
 
-            <div id="infoCard"></div>
+            <div id="infoCard">
+                <div id="categories">
+                    <h3 id="about" class="cursor-pointer" onclick="openAbout()">About</h3>
+                    <h3 id="stats" class="cursor-pointer" onclick="openStats()">Base Stats</h3>
+                </div>
+
+                <div id="categoryContent">
+                
+                </div>
+            </div>
+        </div>
+    `;
+
+    openAbout();
+}
+
+
+function openAbout() {
+    document.getElementById('about').classList.add("onclick");
+    document.getElementById('stats').classList.remove("onclick");
+    let categoryContent = document.getElementById('categoryContent');
+
+    categoryContent.innerHTML = '';
+    categoryContent.innerHTML = `
+        <p id="description" class="mb-24">Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit dolore maiores, qui quasi accusamus possimus magni aut consectetur harum, minus fugit cum laborum in eos magnam. Reiciendis dicta dolorem ad?</p>
+        <div id="heightAndWeight">
+            <div id="height">
+                <p class="color-grey">Height</p>
+                <p>${currentPokemon['height']}</p>
+            </div>
+            <div id="weight">
+                <p class="color-grey">Weight</p>
+                <p>${currentPokemon['weight']}</p>
+            </div>
+        </div>
+    `;
+}
+
+
+function openStats() {
+    document.getElementById('stats').classList.add("onclick");
+    document.getElementById('about').classList.remove("onclick");
+    let categoryContent = document.getElementById('categoryContent');
+
+    categoryContent.innerHTML = '';
+    categoryContent.innerHTML = `
+        <div id="">
+            <div class="display-flex mt-32">
+                <div class="w-124 mb-24">${currentPokemon['stats'][0]['stat']['name']}</div>
+                <div class="w-124 mb-24 jc-end">${currentPokemon['stats'][0]['base_stat']}</div>
+            </div>
+            <div class="display-flex">
+                <div class="w-124 mb-24">${currentPokemon['stats'][1]['stat']['name']}</div>
+                <div class="w-124 jc-end">${currentPokemon['stats'][1]['base_stat']}</div>
+            </div>
+            <div class="display-flex">
+                <div class="w-124 mb-24">${currentPokemon['stats'][2]['stat']['name']}</div>
+                <div class="w-124 mb-24 jc-end">${currentPokemon['stats'][2]['base_stat']}</div>
+            </div>
+            <div class="display-flex">
+                <div class="w-124 mb-24">${currentPokemon['stats'][3]['stat']['name']}</div>
+                <div class="w-124 mb-24 jc-end">${currentPokemon['stats'][3]['base_stat']}</div>
+            </div>
+            <div class="display-flex">
+                <div class="w-124 mb-24">${currentPokemon['stats'][4]['stat']['name']}</div>
+                <div class="w-124 mb-24 jc-end">${currentPokemon['stats'][4]['base_stat']}</div>
+            </div>
+            <div class="display-flex">
+                <div class="w-124 mb-24">${currentPokemon['stats'][5]['stat']['name']}</div>
+                <div class="w-124 mb-24 jc-end">${currentPokemon['stats'][5]['base_stat']}</div>
+            </div>
         </div>
     `;
 }
